f9398b2048ff50f5cb8af6e94026e171bec6e998
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-10-27 14:22:40 +0100

    used flex to make it scrollable on the x axis and added the status badge

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { capitalize } from 'lodash';
 import { Card, CardBody } from "@heroui/card";
 import { Skeleton } from "@heroui/skeleton";
 import { Chip } from "@heroui/chip";
@@ -49,7 +50,7 @@ function BookRecommendations() {
   }
 
   if (!books.length) {
-    return <p className="text-gray-500 mt-6 text-center">No recommendations found.</p>;
+    return <p className="text-gray-500 mt-6 text-center">No recommendations</p>;
   }
 
   return (
@@ -64,24 +65,46 @@ function BookRecommendations() {
 					You Might Also Like 💫
 				</Chip>
 			</div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-4 overflow-x-auto p-2">
+      <div className="flex gap-4 overflow-x-auto px-2 py-4 snap-x snap-mandatory">
         {books.map(book => (
           <Link
             key={book._id}
             to={`/book/${book._id}`}
-            className="group block transition-transform transform hover:scale-[1.01]"
+            className="group block flex-shrink-0 w-[180px] transition-transform transform hover:scale-[1.01] snap-start"
           >
             <Card className="h-full border border-gray-700/50 transition-all duration-300 
 							hover:border-amber-400/50 
 							hover:shadow-[0_0_4px_rgba(251,191,36,0.6)]">
               <CardBody className="p-0 flex flex-col items-center">
-                <img
-                  src={book.bookImage}
-                  alt={book.title}
-									decoding="async"
-									loading="lazy"
-                  className="object-cover w-full h-[220px] rounded-lg"
-                />
+                <div className="relative w-full ">
+                  <img
+                    src={book.bookImage}
+                    alt={book.title}
+                    decoding="async"
+                    loading="lazy"
+                    className="object-cover w-full h-[220px] rounded-lg"
+                  />
+
+                  {/* Status Badge */}
+                  <Chip
+                      color={book.status === 'ongoing' ? 'warning' : 'success'}
+                      variant="flat"
+                      size="sm"
+                      classNames={{
+                      base: "absolute top-4 left-4 uppercase font-bold backdrop-blur-md z-10",
+                      content: book.status === "ongoing" 
+                        ? "text-yellow-500" 
+                        : "text-green-500"
+                    }}
+                  >
+                    {capitalize(book.status)}
+                  </Chip>
+
+                  
+                  {/* Gradient Overlay */}
+                  <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-transparent to-transparent"/>
+                </div>
+
                 <h3 className="text-sm font-semibold text-center my-2 text-gray-800 dark:text-gray-400 line-clamp-2 px-1 group-hover:text-amber-400 group-hover:dark:text-amber-400  transition-colors duration-300">
                   {startCase(book.title)}
                 </h3>
