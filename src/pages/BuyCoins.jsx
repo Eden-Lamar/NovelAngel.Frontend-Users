801680c94e32cee2a3d678673f55c54939c6163f
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-11-05 10:08:20 +0100

    Changed the container padding to 10px

--- src/pages/BuyCoins.jsx
+++ src/pages/BuyCoins.jsx
@@ -102,7 +102,7 @@ function BuyCoins() {
     };
 
     return (
-        <div className="container mx-auto px-4 py-8">
+        <div className="container mx-auto px-10 py-8">
             {/* Error Alert */}
             {error && (
 							<AlertMessage message={error} onClose={() => setError(null)} />
